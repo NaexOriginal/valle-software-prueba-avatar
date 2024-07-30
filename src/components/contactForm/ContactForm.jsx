a12238a0b14f0c4f11ccefc3a26160f51239b407
Naex <rafaelpiedrahita414@gmail.com>
2024-07-30 08:49:01 -0500

    Necesario mejorar seccion de equipo y metodo de contacto

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -3,31 +3,31 @@ export const ContactForm = () => {
   return (
     <div className="p-5">
       <h2 className="text-start text-6xl">Contactanos</h2>
-      <p className="pt-5 text-xl">Contacta con nosotros para ver en que podemos coloborarte</p>
+      <p className="pt-5 text-xl mb-5">Contacta con nosotros para ver en que podemos coloborarte</p>
 
       <div className="flex flex-row">
         <form action="submit">
-          <span>
-            <input type="text" placeholder="Primer Nombre" className=""/>
-            <input type="text" placeholder="Segundo Nombre" className="border-gray-950"/>
-          </span>
+          <div className="flex flex-wrap gap-4 mb-4">
+            <input type="text" placeholder="Primer Nombre" className="flex-grow p-2 border rounded"/>
+            <input type="text" placeholder="Segundo Nombre" className="flex-grow p-2 border rounded"/>
+          </div>
 
-          <span>
-            <input type="email" placeholder="Correo Electronico"/>
-            <input type="number" placeholder="Telefono"/>
-          </span>
+          <div className="flex flex-wrap gap-4 mb-4">
+            <input type="email" placeholder="Correo Electronico" className="flex-grow p-2 border rounded"/>
+            <input type="number" placeholder="Telefono" className="flex-grow p-2 border rounded"/>
+          </div>
 
-          <span>
-            <select>
+          <div className="mb-4">
+            <select className="w-full p-2 border rounded">
               <option value="">--Por favor, seleccione una opción--</option>
               <option value="Servicios">Servicios</option>
             </select>
-          </span>
+          </div>
 
-          <span>
-            <textarea placeholder="Mensaje"></textarea>
-          </span>
-          <button type="submit">Enviar</button>
+          <div>
+            <textarea placeholder="Mensaje" className="w-full p-2 border rounded" />
+          </div>
+          <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded">Enviar</button>
         </form>
 
       </div>
