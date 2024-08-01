3cf679a11d9532d58f92381242d784dc5c278fc5
jony24a <127144327+jony24a@users.noreply.github.com>
2024-07-31 22:14:50 -0500

    Arreglos de Landing y CardEmployed

--- src/components/cardEmployed/CardEmployed.jsx
+++ src/components/cardEmployed/CardEmployed.jsx
@@ -1,12 +1,136 @@
-
 export const CardEmployed = () => {
   return (
-    <div id="Equipo" className="flex flex-col p-5 items-center min-h-screen">
-        <h2 className="text-center text-6xl pb-14">Equipo</h2>
+    <div id="Equipo" className="flex flex-wrap p-5 items-center min-h-screen justify-center">
+        <h2 className="text-5xl md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800 mb-12 text-center text-6xl pb-14 w-full ">Equipo</h2>
       
-      <div className="max-w-xs bg-slate-900 border border-gray-800 text-white p-6 rounded-lg shadow-lg">
+      <div className="max-w-xs bg-slate-900 border border-gray-800 text-white p-6 rounded-lg shadow-lg m-4">
         <div className="flex items-center justify-between">
-          
+          <img src="/LogoValleDelSoftware.svg" alt="Logo" className="w-15 h-12 transform" />
+        </div>
+        <div className="mt-4">
+          <div className="flex items-center justify-center mb-2">
+            <span className="p-2 rounded-full bg-violet-600">
+              <img src="" alt="Foto" className="w-16 h-16 rounded-full" />
+            </span>
+          </div>
+          <p className="text-center italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quisquam nulla optio?</p>
+          <div className="mt-4 text-center">
+            <p className="font-bold">David Felipe Vaszquez Pardo</p>
+            <p className="text-sm">Junior Software Dev</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-xs bg-slate-900 border border-gray-800 text-white p-6 rounded-lg shadow-lg m-4">
+        <div className="flex items-center justify-between">
+          <img src="/LogoValleDelSoftware.svg" alt="Logo" className="w-15 h-12 transform" />
+        </div>
+        <div className="mt-4">
+          <div className="flex items-center justify-center mb-2">
+            <span className="p-2 rounded-full bg-violet-600">
+              <img src="" alt="Foto" className="w-16 h-16 rounded-full" />
+            </span>
+          </div>
+          <p className="text-center italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quisquam nulla optio?</p>
+          <div className="mt-4 text-center">
+            <p className="font-bold">Angel Daniel Diaz Bastidas</p>
+            <p className="text-sm">Junior Software Dev</p>
+          </div>
+        </div>
+      </div>
+      
+      <div className="max-w-xs bg-slate-900 border border-gray-800 text-white p-6 rounded-lg shadow-lg m-4">
+        <div className="flex items-center justify-between">
+          <img src="/LogoValleDelSoftware.svg" alt="Logo" className="w-15 h-12 transform" />
+        </div>
+        <div className="mt-4">
+          <div className="flex items-center justify-center mb-2">
+            <span className="p-2 rounded-full bg-violet-600">
+              <img src="" alt="Foto" className="w-16 h-16 rounded-full" />
+            </span>
+          </div>
+          <p className="text-center italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quisquam nulla optio?</p>
+          <div className="mt-4 text-center">
+            <p className="font-bold">Jhon Deivid Ramirez Nuñez</p>
+            <p className="text-sm">Junior Software Dev</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-xs bg-slate-900 border border-gray-800 text-white p-6 rounded-lg shadow-lg m-4">
+        <div className="flex items-center justify-between">
+          <img src="/LogoValleDelSoftware.svg" alt="Logo" className="w-15 h-12 transform" />
+        </div>
+        <div className="mt-4">
+          <div className="flex items-center justify-center mb-2">
+            <span className="p-2 rounded-full bg-violet-600">
+              <img src="" alt="Foto" className="w-16 h-16 rounded-full" />
+            </span>
+          </div>
+          <p className="text-center italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quisquam nulla optio?</p>
+          <div className="mt-4 text-center">
+            <p className="font-bold">Jonatan Andres Novoa Rodriguez</p>
+            <p className="text-sm">Junior Software Dev</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-xs bg-slate-900 border border-gray-800 text-white p-6 rounded-lg shadow-lg m-4">
+        <div className="flex items-center justify-between">
+          <img src="/LogoValleDelSoftware.svg" alt="Logo" className="w-15 h-12 transform" />
+        </div>
+        <div className="mt-4">
+          <div className="flex items-center justify-center mb-2">
+            <span className="p-2 rounded-full bg-violet-600">
+              <img src="" alt="Foto" className="w-16 h-16 rounded-full" />
+            </span>
+          </div>
+          <p className="text-center italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quisquam nulla optio?</p>
+          <div className="mt-4 text-center">
+            <p className="font-bold">Daniel Felipe Ortega Gutierrez</p>
+            <p className="text-sm">Junior Software Dev</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-xs bg-slate-900 border border-gray-800 text-white p-6 rounded-lg shadow-lg m-4">
+        <div className="flex items-center justify-between">
+          <img src="/LogoValleDelSoftware.svg" alt="Logo" className="w-15 h-12 transform" />
+        </div>
+        <div className="mt-4">
+          <div className="flex items-center justify-center mb-2">
+            <span className="p-2 rounded-full bg-violet-600">
+              <img src="" alt="Foto" className="w-16 h-16 rounded-full" />
+            </span>
+          </div>
+          <p className="text-center italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quisquam nulla optio?</p>
+          <div className="mt-4 text-center">
+            <p className="font-bold">Rafael Leonardo Piedrahita Correa</p>
+            <p className="text-sm">Junior Software Dev</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-xs bg-slate-900 border border-gray-800 text-white p-6 rounded-lg shadow-lg m-4">
+        <div className="flex items-center justify-between">
+          <img src="/LogoValleDelSoftware.svg" alt="Logo" className="w-15 h-12 transform" />
+        </div>
+        <div className="mt-4">
+          <div className="flex items-center justify-center mb-2">
+            <span className="p-2 rounded-full bg-violet-600">
+              <img src="" alt="Foto" className="w-16 h-16 rounded-full" />
+            </span>
+          </div>
+          <p className="text-center italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quisquam nulla optio?</p>
+          <div className="mt-4 text-center">
+            <p className="font-bold">Leonel Lugo Mendoza</p>
+            <p className="text-sm">Junior Software Dev</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="max-w-xs bg-slate-900 border border-gray-800 text-white p-6 rounded-lg shadow-lg m-4">
+        <div className="flex items-center justify-between">
           <img src="/LogoValleDelSoftware.svg" alt="Logo" className="w-15 h-12 transform" />
         </div>
         <div className="mt-4">
@@ -22,6 +146,7 @@ export const CardEmployed = () => {
           </div>
         </div>
       </div>
+
     </div>
   )
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,8 +1,8 @@
-import { Avatar } from "../components/avatar/Avatar"
-import { CardEmployed } from "../components/cardEmployed/CardEmployed"
-import { ContactForm } from "../components/contactForm/ContactForm"
-import { Footer } from "../components/footer/Footer"
-import { NavBarSticky } from "../components/navbar/NavBarSticky"
+import { Avatar } from "../components/avatar/Avatar";
+import { CardEmployed } from "../components/cardEmployed/CardEmployed";
+import { ContactForm } from "../components/contactForm/ContactForm";
+import { Footer } from "../components/footer/Footer";
+import { NavBarSticky } from "../components/navbar/NavBarSticky";
 
 export const LandingPage = () => {
   return (
@@ -10,50 +10,80 @@ export const LandingPage = () => {
       <NavBarSticky />
       <Avatar />
 
+      <div id="Sobre-Nosotros" className="min-h-screen flex flex-col items-center justify-center">
+        <h2 className="text-center md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800 mb-24">
+          Sobre Nosotros
+        </h2>
 
-      <div id="Sobre-Nosotros" className="min-h-screen">
-        <h2 className="text-center md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800 mb-12 ">Sobre Nosotros</h2>
-
-        <div className="justify-center flex flex-wrap p-8 bg-gray-950">
-          <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-2xl mx-auto">
-            <h3 className="text-4xl text-center mb-5 text-white">Nuestra Misión</h3>
-            <p className="text-center">En "El valle del software", nuestra misión es innovar y transformar el panorama tecnológico mediante el desarrollo de soluciones de inteligencia artificial de vanguardia. Nos dedicamos a crear proyectos que impulsen la eficiencia y la evolución en diversas áreas del mercado tecnológico, proporcionando herramientas inteligentes que potencien el crecimiento y la competitividad de nuestros clientes. Trabajamos con pasión y compromiso para convertir ideas en realidades que cambian el mundo.</p>
+        <div className="flex flex-col items-center justify-center w-full p-8 bg-gray-950">
+          <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-3xl mx-auto mb-8 max-h-96 overflow-y-auto">
+            <h3 className="text-5xl text-center mb-5 text-white">Nuestra Misión</h3>
+            <p className="text-center text-lg">
+              En El valle del software, nuestra misión es innovar y transformar
+              el panorama tecnológico mediante el desarrollo de soluciones de
+              inteligencia artificial de vanguardia. Nos dedicamos a crear
+              proyectos que impulsen la eficiencia y la evolución en diversas
+              áreas del mercado tecnológico, proporcionando herramientas
+              inteligentes que potencien el crecimiento y la competitividad de
+              nuestros clientes. Trabajamos con pasión y compromiso para
+              convertir ideas en realidades que cambian el mundo.
+            </p>
           </div>
 
-          <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-2xl mx-auto">
-            <h3 className="text-4xl text-center mb-5">Nuestra Visión</h3>
-            <p className="text-center">Nuestra visión en "El valle del software" es ser reconocidos como líderes globales en el desarrollo de tecnologías de inteligencia artificial, estableciendo nuevos estándares de excelencia e innovación en la industria. Aspiramos a construir un ecosistema donde la creatividad y la tecnología se entrelacen, generando un impacto positivo y duradero en el futuro del mercado tecnológico. Queremos ser el puente que conecta a las empresas con soluciones inteligentes que transformen su manera de operar y crecer.</p>
+          <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-3xl mx-auto mb-8 max-h-96 overflow-y-auto">
+            <h3 className="text-5xl text-center mb-5 text-white">Nuestra Visión</h3>
+            <p className="text-center text-lg">
+              Nuestra visión en El valle del software es ser reconocidos como
+              líderes globales en el desarrollo de tecnologías de inteligencia
+              artificial, estableciendo nuevos estándares de excelencia e
+              innovación en la industria. Aspiramos a construir un ecosistema
+              donde la creatividad y la tecnología se entrelacen, generando un
+              impacto positivo y duradero en el futuro del mercado tecnológico.
+              Queremos ser el puente que conecta a las empresas con soluciones
+              inteligentes que transformen su manera de operar y crecer.
+            </p>
           </div>
         </div>
       </div>
 
-      <div 
-        id="Servicios"
-        className="p-5 min-h-screen"
-      >
-        <h2 className="text-center text-6xl">Servicios</h2>
-        
+      <div id="Servicios" className="p-5 min-h-screen">
+        <h2 className="text-center md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800 mb-12">Servicios</h2>
+
         <div className="flex flex-col pt-14 space-y-8">
-          <div className="max-w-2xl mx-auto">
+          <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-2xl mx-auto">
             <h3 className="text-center text-4xl">Desarrollo Web</h3>
-            <p className="text-center">Creamos páginas web innovadoras, integrando inteligencia artificial para llevar tu negocio al siguiente nivel en el mercado tecnológico.</p>
+            <p className="text-center">
+              Creamos páginas web innovadoras, integrando inteligencia
+              artificial para llevar tu negocio al siguiente nivel en el mercado
+              tecnológico.
+            </p>
           </div>
 
-          <div className="max-w-2xl mx-auto">
-            <h3 className="text-center text-4xl">Aplicaciones Moviles</h3>
-            <p className="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis saepe, illo modi ea magnam ex hic. Aliquid, necessitatibus vitae perspiciatis a cum perferendis, eveniet amet deleniti tempore ullam, assumenda nulla.</p>
+          <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-2xl mx-auto">
+            <h3 className="text-center text-4xl">Aplicaciones Móviles</h3>
+            <p className="text-center">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Perferendis saepe, illo modi ea magnam ex hic. Aliquid,
+              necessitatibus vitae perspiciatis a cum perferendis, eveniet amet
+              deleniti tempore ullam, assumenda nulla.
+            </p>
           </div>
 
-          <div className="max-w-2xl mx-auto">
+          <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-2xl mx-auto">
             <h3 className="text-center text-4xl">Otros</h3>
-            <p className="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime dignissimos at aspernatur ipsa odit officia accusamus excepturi id alias nobis ex est expedita iure reprehenderit magni quam, suscipit quos commodi.</p>
+            <p className="text-center">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime
+              dignissimos at aspernatur ipsa odit officia accusamus excepturi id
+              alias nobis ex est expedita iure reprehenderit magni quam,
+              suscipit quos commodi.
+            </p>
           </div>
         </div>
       </div>
-      
+
       <CardEmployed />
       <ContactForm />
       <Footer />
     </div>
-  )
-}
+  );
+};
