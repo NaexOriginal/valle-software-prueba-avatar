7f3b02941d665b0d4431e2616427a9f6465b67b5
Naex <rafaelpiedrahita414@gmail.com>
2024-08-06 14:07:21 -0500

    SVG y gradiente

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -2,40 +2,64 @@ import '../ui/background.css'
 
 export const AboutUs = () => {
   return (
-    <div id="Sobre-Nosotros" className="bg-black-custom min-h-screen flex flex-col items-center justify-center ">
-      <h2 className="text-center md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800 mb-24">
+    <section id="Sobre-Nosotros" className="bg-black-custom min-h-screen flex flex-col items-center justify-center ">
+      <h2 className="text-center md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800 mb-8">
         Sobre Nosotros
       </h2>
 
-      <div className="flex flex-col items-center justify-center w-full p-8">
-        <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-3xl mx-auto mb-8 max-h-96 overflow-y-auto">
-          <h3 className="text-5xl text-center mb-5 text-white">Nuestra Misión</h3>
-          <p className="text-center text-lg">
-            En El valle del software, nuestra misión es innovar y transformar
-            el panorama tecnológico mediante el desarrollo de soluciones de
-            inteligencia artificial de vanguardia. Nos dedicamos a crear
-            proyectos que impulsen la eficiencia y la evolución en diversas
-            áreas del mercado tecnológico, proporcionando herramientas
-            inteligentes que potencien el crecimiento y la competitividad de
-            nuestros clientes. Trabajamos con pasión y compromiso para
-            convertir ideas en realidades que cambian el mundo.
-          </p>
+      <div className="flex flex-row items-center justify-evenly w-full p-8">
+        <div className='relative group'>
+          <div className='absolute inset-0 bg-gradient-to-r from-sky-600 to-blue-700 rounded-lg blur-md'></div>
+          <div className="relative bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-3xl max-h-svh mx-auto mb-8 overflow-y-auto">
+            <h3 className="text-5xl text-center text-white">Nuestra Misión</h3>
+            
+            <div className='flex items-center justify-center'>
+              <img 
+                src="/Objetivo.svg" 
+                alt="Logo Misión" 
+                className='w-36 h-36'
+              />
+            </div>
+
+            <p className="text-center text-lg">
+              En El valle del software, nuestra misión es innovar y transformar
+              el panorama tecnológico mediante el desarrollo de soluciones de
+              inteligencia artificial de vanguardia. Nos dedicamos a crear
+              proyectos que impulsen la eficiencia y la evolución en diversas
+              áreas del mercado tecnológico, proporcionando herramientas
+              inteligentes que potencien el crecimiento y la competitividad de
+              nuestros clientes. Trabajamos con pasión y compromiso para
+              convertir ideas en realidades que cambian el mundo.
+            </p>
+          </div>
         </div>
 
-        <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-3xl mx-auto mb-8 max-h-96 overflow-y-auto">
-          <h3 className="text-5xl text-center mb-5 text-white">Nuestra Visión</h3>
-          <p className="text-center text-lg">
-            Nuestra visión en El valle del software es ser reconocidos como
-            líderes globales en el desarrollo de tecnologías de inteligencia
-            artificial, estableciendo nuevos estándares de excelencia e
-            innovación en la industria. Aspiramos a construir un ecosistema
-            donde la creatividad y la tecnología se entrelacen, generando un
-            impacto positivo y duradero en el futuro del mercado tecnológico.
-            Queremos ser el puente que conecta a las empresas con soluciones
-            inteligentes que transformen su manera de operar y crecer.
-          </p>
+        <div className='relative group'>
+          <div className='absolute inset-0 bg-gradient-to-l from-sky-600 to-blue-700 rounded-lg blur-md'></div>
+          <div className='relative bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-3xl max-h-svh mx-auto mb-8 overflow-y-auto'>
+            <h3 className="text-5xl text-center text-white">Nuestra Visión</h3>
+
+            <div className='flex items-center justify-center'>
+              <img 
+                src='/Mision.svg'
+                alt='Logo Visión'
+                className='w-36 h-36'
+              />
+            </div>
+
+            <p className="text-center text-lg">
+              Nuestra visión en El valle del software es ser reconocidos como
+              líderes globales en el desarrollo de tecnologías de inteligencia
+              artificial, estableciendo nuevos estándares de excelencia e
+              innovación en la industria. Aspiramos a construir un ecosistema
+              donde la creatividad y la tecnología se entrelacen, generando un
+              impacto positivo y duradero en el futuro del mercado tecnológico.
+              Queremos ser el puente que conecta a las empresas con soluciones
+              inteligentes que transformen su manera de operar y crecer.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -16,7 +16,7 @@ export const LandingPage = () => {
 
 
         <div id="Servicios" className="p-5 min-h-screen bg-gray-950 ">
-          <h2 className="text-center md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800 mb-12">Servicios</h2>
+          <h2 className="text-center md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-800 mb-6">Servicios</h2>
 
           <div className="flex flex-col pt-14 space-y-8">
             <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-2xl mx-auto">
@@ -26,6 +26,16 @@ export const LandingPage = () => {
                 artificial para llevar tu negocio al siguiente nivel en el mercado
                 tecnológico.
               </p>
+
+              <div className="mt-6 flex justify-center items-center">
+                <span className="p-2 rounded-full bg-slate-50">
+                  <img
+                    src="/Desarrollo-Web.svg" 
+                    alt="Logo desarrollo web" 
+                    className="h-36 w-36 rounded-full"
+                  />
+                </span>
+              </div>
             </div>
 
             <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-2xl mx-auto">
@@ -36,6 +46,16 @@ export const LandingPage = () => {
                 necessitatibus vitae perspiciatis a cum perferendis, eveniet amet
                 deleniti tempore ullam, assumenda nulla.
               </p>
+
+              <div className="mt-6 flex justify-center items-center">
+                <span className="p-2 rounded-full bg-slate-50">
+                  <img
+                    src="/Movil.svg" 
+                    alt="Logo Desarrollo Movil" 
+                    className="h-36 w-36 rounded-full"
+                  />
+                </span>
+              </div>
             </div>
 
             <div className="bg-gray-900 border border-gray-800 p-8 rounded-lg shadow-lg w-full max-w-2xl mx-auto">
@@ -46,6 +66,16 @@ export const LandingPage = () => {
                 alias nobis ex est expedita iure reprehenderit magni quam,
                 suscipit quos commodi.
               </p>
+
+              <div className="mt-6 flex justify-center items-center">
+                <span className="p-2 rounded-full bg-slate-50">
+                  <img
+                    src="/Entre-Otros.svg" 
+                    alt="Logo Desarrollo Extra" 
+                    className="h-36 w-36 rounded-full"
+                  />
+                </span>
+              </div>
             </div>
           </div>
         </div>
