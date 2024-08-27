0824d32c80693439c934972f5f7e9a842b91fb00
jony24a <127144327+jony24a@users.noreply.github.com>
2024-08-26 20:57:32 -0500

    Diseno de avatar, falta volverlo responsive

--- src/components/avatar/AvatarCanvas.jsx
+++ src/components/avatar/AvatarCanvas.jsx
@@ -8,16 +8,25 @@ const Character = () => {
     <group>
       <primitive object={character.scene} />
     </group>
-  )
-}
+  );
+};
 
 export const AvatarCanvas = () => {
   return (
-    <Canvas style={{width: '600px', height: '500px'}}>
-      <ambientLight intensity={3}/>
-      <PerspectiveCamera makeDefault position={[0, 1.7, 1]}/>
-      <OrbitControls target={[0, 1.43, 0]} />
-      <Character  />
-    </Canvas>
-  )
-}
+    <div style={{
+      width: '620px', 
+      height: '520px', 
+      padding: '10px', 
+      borderRadius: '15px', 
+      border: '3px solid #3498db', /* Color del borde */
+      boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)' /* Sombra para destacar un poco */
+    }}>
+      <Canvas style={{ width: '600px', height: '500px' }}>
+        <ambientLight intensity={3} />
+        <PerspectiveCamera makeDefault position={[0, 1.7, 1]} />
+        <OrbitControls target={[0, 1.43, 0]} />
+        <Character />
+      </Canvas>
+    </div>
+  );
+};
