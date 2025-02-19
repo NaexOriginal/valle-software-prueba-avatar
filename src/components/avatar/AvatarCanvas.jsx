2be24b727a4044bac5cf0ee8aa6dbbb1e07b4eb3
jony24a <jeijei24a@gmail.com>
2025-02-19 15:11:56 -0600

    Cambio de Avatar

--- src/components/avatar/AvatarCanvas.jsx
+++ src/components/avatar/AvatarCanvas.jsx
@@ -13,20 +13,15 @@ const Character = () => {
 
 export const AvatarCanvas = () => {
   return (
-    <div style={{
-      width: '620px', 
-      height: '520px', 
-      padding: '10px', 
-      borderRadius: '15px', 
-      border: '3px solid #3498db', /* Color del borde */
-      boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)' /* Sombra para destacar un poco */
-    }}>
-      <Canvas style={{ width: '600px', height: '500px' }}>
-        <ambientLight intensity={3} />
-        <PerspectiveCamera makeDefault position={[0, 1.7, 1]} />
-        <OrbitControls target={[0, 1.43, 0]} />
-        <Character />
-      </Canvas>
+    <div className="w-full max-w-lg h-[520px] p-3 rounded-xl border-2 border-blue-500 shadow-md">
+      <div className="w-full h-full">
+        <Canvas className="w-full max-w-lg h-[520px] ">
+          <ambientLight intensity={3} />
+          <PerspectiveCamera makeDefault position={[0, 1.7, 1]} />
+          <OrbitControls target={[0, 1.43, 0]} />
+          <Character />
+        </Canvas>
+      </div>
     </div>
   );
 };
